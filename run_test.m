6b8e798ab62e039b26e3f981944529002c4b8950
Anil Bollimunta <31591337+bollimunta@users.noreply.github.com>
2017-09-05 13:50:51 -0400

    Update run_test.m
    added comments

--- run_test.m
+++ run_test.m
@@ -1,2 +1,2 @@
-[s]=poissontrains_ramp(100,0,20,0.3);
-global_ratio_data(s)
+[s]=poissontrains_ramp(100,0,20,0.3); % generate test data that is ramping in firing rate
+global_ratio_data(s) % run model selection
